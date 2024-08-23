7b8be06910819a51890d06d43ebf68fb7982166c
JawadAltaf <jawadaltaf305@gmail.com>
2024-08-23 16:20:28 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('auth')->group(function(){
+
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class)->middleware('guest');
+    Route::post('/register', RegisterController::class)->middleware('guest');
+
+});
